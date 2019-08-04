4174cc5f51b4a590ada2ab382bdc130c7558ebeb
holdwhatdoor <sbouch4@wgu.edu>
2019-08-03 23:18:33 -0700

    Created TermEditAdapter class and copied code from TermItemAdapter class and pasted.  Changed method/class names appropriately. Imported static Constants for course data to TermEditAdapter class.
    Fixed date format appearance for edit text fields in TermEdit page.  Now reflect MM/dd/yyyy format

---
+++ app/src/main/java/com/example/c196project/ui/TermEditAdapter.java
@@ -0,0 +1,187 @@
+package com.example.c196project.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.c196project.R;
+import com.example.c196project.TermEdit;
+import com.example.c196project.database.CourseEntity;
+import com.example.c196project.database.DateConverter;
+
+import java.util.Date;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.example.c196project.utilities.Constants.COURSE_EMAIL_KEY;
+import static com.example.c196project.utilities.Constants.COURSE_END_KEY;
+import static com.example.c196project.utilities.Constants.COURSE_ID_KEY;
+import static com.example.c196project.utilities.Constants.COURSE_MENTOR_KEY;
+import static com.example.c196project.utilities.Constants.COURSE_NOTES_KEY;
+import static com.example.c196project.utilities.Constants.COURSE_PHONE_KEY;
+import static com.example.c196project.utilities.Constants.COURSE_START_KEY;
+import static com.example.c196project.utilities.Constants.COURSE_STATUS_KEY;
+import static com.example.c196project.utilities.Constants.COURSE_TITLE_KEY;
+import static com.example.c196project.utilities.Constants.TERM_ID_KEY;
+
+
+public class TermEditAdapter extends RecyclerView.Adapter<TermEditAdapter.TermEditHolder> {
+    String TAG = "TermEditAdapter";
+
+    private final List<CourseEntity> mCourses;
+    private final Context mContext;
+
+    @BindView(R.id.rv_term_list)
+    public RecyclerView termRV;
+
+    public TermEditAdapter(List<CourseEntity> mCourses, Context mContext) {
+        this.mCourses = mCourses;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public TermEditHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.layout_itemlist, parent, false);
+        return new TermEditHolder(view);
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull TermEditHolder holder, int position) {
+
+        final CourseEntity term = mCourses.get(position);
+
+        holder.mListItem.setText(term.getCourseTitle());
+        holder.mStartDate.setText(DateConverter.formatDateString(term.getStartDate().toString()));
+        holder.mEndDate.setText(DateConverter.formatDateString(term.getEndDate().toString()));
+
+        holder.mDelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int courseId = getCourseAtPos(position).getTermId();
+                String courseTitle = getCourseAtPos(position).getCourseTitle();
+                Date start = getCourseAtPos(position).getStartDate();
+                Date end = getCourseAtPos(position).getEndDate();
+                String status = getCourseAtPos(position).getStatus();
+                String mentor = getCourseAtPos(position).getMentorName();
+                String mentorPhone = getCourseAtPos(position).getMentorPhone();
+                String mentorEmail = getCourseAtPos(position).getMentorEmail();
+                String notes = getCourseAtPos(position).getCourseNotes();
+                int termId = getCourseAtPos(position).getTermId();
+
+
+                CourseEntity course = new CourseEntity(courseId, courseTitle, start, end, status,
+                        mentor, mentorPhone, mentorEmail, notes, termId);
+
+
+                Log.d(TAG, "Course ID: " + courseId);
+                Log.d(TAG, "Course Title: " + courseTitle);
+                Log.d(TAG, "Course Start Date: " + start);
+                Log.d(TAG, "Course End Date: " + end);
+                Log.d(TAG, "Course Data: " + course.toString());
+
+                Intent intent = new Intent(mContext, TermEdit.class);
+                intent.putExtra(TERM_ID_KEY, term.getTermId());
+                mContext.startActivity(intent);
+            }
+        });
+
+        holder.mEditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int courseId = getCourseAtPos(position).getTermId();
+                String courseTitle = getCourseAtPos(position).getCourseTitle();
+                Date start = getCourseAtPos(position).getStartDate();
+                Date end = getCourseAtPos(position).getEndDate();
+                String status = getCourseAtPos(position).getStatus();
+                String mentor = getCourseAtPos(position).getMentorName();
+                String mentorPhone = getCourseAtPos(position).getMentorPhone();
+                String mentorEmail = getCourseAtPos(position).getMentorEmail();
+                String notes = getCourseAtPos(position).getCourseNotes();
+                int termId = getCourseAtPos(position).getTermId();
+
+                CourseEntity course = new CourseEntity(courseId, courseTitle, start, end, status,
+                        mentor, mentorPhone, mentorEmail, notes, termId);
+
+                Log.d(TAG, "Course ID: " + courseId);
+                Log.d(TAG, "Course Title: " + courseTitle);
+                Log.d(TAG, "Course Start Date: " + start);
+                Log.d(TAG, "Term End Date: " + end);
+                Log.d(TAG, "Term Data: " + term.toString());
+
+                Intent intent = new Intent(mContext, TermEdit.class);
+                intent.putExtra(COURSE_ID_KEY, course.getTermId());
+                intent.putExtra(COURSE_TITLE_KEY, course.getCourseTitle());
+                intent.putExtra(COURSE_START_KEY, course.getStartDate().toString());
+                intent.putExtra(COURSE_END_KEY, course.getEndDate().toString());
+                intent.putExtra(COURSE_STATUS_KEY, course.getEndDate().toString());
+                intent.putExtra(COURSE_MENTOR_KEY, course.getEndDate().toString());
+                intent.putExtra(COURSE_PHONE_KEY, course.getEndDate().toString());
+                intent.putExtra(COURSE_EMAIL_KEY, course.getEndDate().toString());
+                intent.putExtra(COURSE_NOTES_KEY, course.getEndDate().toString());
+
+                mContext.startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TermEditHolder holder, int position, @NonNull List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+
+    }
+
+    public CourseEntity getCourseAtPos(int position){
+
+        return mCourses != null ? mCourses.get(position) :null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mCourses.size();
+    }
+
+    public class TermEditHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.list_item)
+        TextView mListItem;
+        @BindView(R.id.start_date)
+        TextView mStartDate;
+        @BindView(R.id.end_date)
+        TextView mEndDate;
+        @BindView(R.id.item_del_btn)
+        Button mDelBtn;
+        @BindView(R.id.item_edit_btn)
+        Button mEditBtn;
+
+
+        public TermEditHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+        }
+    }
+
+    public void removeAt(int position) {
+
+        mCourses.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, mCourses.size());
+    }
+}
+
+
---
+++ app/src/main/java/com/example/c196project/ui/TermItemAdapter.java
@@ -0,0 +1,154 @@
+package com.example.c196project.ui;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.c196project.R;
+import com.example.c196project.TermEdit;
+import com.example.c196project.database.DateConverter;
+import com.example.c196project.database.TermEntity;
+
+import java.util.Date;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+import static com.example.c196project.utilities.Constants.TERM_END_KEY;
+import static com.example.c196project.utilities.Constants.TERM_ID_KEY;
+import static com.example.c196project.utilities.Constants.TERM_START_KEY;
+import static com.example.c196project.utilities.Constants.TERM_TITLE_KEY;
+
+public class TermItemAdapter extends RecyclerView.Adapter<TermItemAdapter.TermHolder> {
+    String TAG = "TermItemAdapter";
+
+    private final List<TermEntity> mTerms;
+    private final Context mContext;
+
+    @BindView(R.id.rv_term_list)
+    public RecyclerView termRV;
+
+    public TermItemAdapter(List<TermEntity> mTerms, Context mContext) {
+        this.mTerms = mTerms;
+        this.mContext = mContext;
+    }
+
+    @NonNull
+    @Override
+    public TermHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.layout_itemlist, parent, false);
+        return new TermHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TermHolder holder, int position) {
+
+        final TermEntity term = mTerms.get(position);
+
+        holder.mListItem.setText(term.getTermTitle());
+        holder.mStartDate.setText(DateConverter.formatDateString(term.getStart().toString()));
+        holder.mEndDate.setText(DateConverter.formatDateString(term.getEnd().toString()));
+
+        holder.mDelBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int termId = getTermAtPos(position).getTermId();
+                String termTitle = getTermAtPos(position).getTermTitle();
+                Date start = getTermAtPos(position).getStart();
+                Date end = getTermAtPos(position).getEnd();
+                TermEntity term = new TermEntity(termId, termTitle, start, end);
+
+                Log.d(TAG, "Term ID: " + termId);
+                Log.d(TAG, "Term Title: " + termTitle);
+                Log.d(TAG, "Term Start Date: " + start);
+                Log.d(TAG, "Term End Date: " + end);
+                Log.d(TAG, "Term Data: " + term.toString());
+
+                Intent intent = new Intent(mContext, TermEdit.class);
+                intent.putExtra(TERM_ID_KEY, term.getTermId());
+                mContext.startActivity(intent);
+            }
+        });
+
+        holder.mEditBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int termId = TermItemAdapter.this.getTermAtPos(position).getTermId();
+                String termTitle = TermItemAdapter.this.getTermAtPos(position).getTermTitle();
+                Date start = TermItemAdapter.this.getTermAtPos(position).getStart();
+                Date end = TermItemAdapter.this.getTermAtPos(position).getEnd();
+                TermEntity term = new TermEntity(termId, termTitle, start, end);
+
+                Log.d(TAG, "Term ID: " + termId);
+                Log.d(TAG, "Term Title: " + termTitle);
+                Log.d(TAG, "Term Start Date: " + start);
+                Log.d(TAG, "Term End Date: " + end);
+                Log.d(TAG, "Term Data: " + term.toString());
+
+                Intent intent = new Intent(mContext, TermEdit.class);
+                intent.putExtra(TERM_ID_KEY, term.getTermId());
+                intent.putExtra(TERM_TITLE_KEY, term.getTermTitle());
+                intent.putExtra(TERM_START_KEY, term.getStart().toString());
+                intent.putExtra(TERM_END_KEY, term.getEnd().toString());
+                mContext.startActivity(intent);
+
+            }
+        });
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull TermHolder holder, int position, @NonNull List<Object> payloads) {
+        super.onBindViewHolder(holder, position, payloads);
+
+    }
+
+    public TermEntity getTermAtPos(int position){
+
+        return mTerms != null ? mTerms.get(position) :null;
+    }
+
+    @Override
+    public int getItemCount() {
+        return mTerms.size();
+    }
+
+    public class TermHolder extends RecyclerView.ViewHolder {
+        @BindView(R.id.list_item)
+        TextView mListItem;
+        @BindView(R.id.start_date)
+        TextView mStartDate;
+        @BindView(R.id.end_date)
+        TextView mEndDate;
+        @BindView(R.id.item_del_btn)
+        Button mDelBtn;
+        @BindView(R.id.item_edit_btn)
+        Button mEditBtn;
+
+
+        public TermHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+
+        }
+    }
+
+    public void removeAt(int position) {
+
+        mTerms.remove(position);
+        notifyItemRemoved(position);
+        notifyItemRangeChanged(position, mTerms.size());
+    }
+}
+
